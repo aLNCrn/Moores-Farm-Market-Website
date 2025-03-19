4a36cb17a0b214c8396323f187410307ec858c2b
aLNCrn <cerona1@go.stockton.edu>
2025-03-19 12:45:15 -0400

    changed literallty everything
    ID ONTKNWO

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')  # Explicitly set templates folder
 
 # Secret Key for session security
-app.secret_key = 'your_secret_key'
+app.secret_key = 'mfmpass'
 
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
@@ -16,7 +16,6 @@ app.config['MYSQL_DB'] = 'mooresfarmmarket'
 
 # Initialize MySQL
 mysql = MySQL(app)
-
 
 # Test Database Connection
 @app.route('/test_db')
@@ -30,39 +29,35 @@ def test_db():
     except Exception as e:
         return str(e)
 
-
 # Home Route
 @app.route('/home')
 def home():
     if 'loggedin' in session:
-        return render_template('home.html', username=session['email'], email=session['email'])
+        return render_template('home.html', username=session['usrname'], email=session['email'])
     return redirect(url_for('login'))
-
 
 # Login Route
 @app.route('/', methods=['GET', 'POST'])
 def login():
     msg = ''
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        username = request.form['email']
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
         password = request.form['password']
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM Customer WHERE email = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM MyGuests WHERE usrname = %s AND password = %s', (username, password,))
         account = cursor.fetchone()
 
         if account:
             session['loggedin'] = True
-            session['Customerid'] = account['Customerid']
-            session['FirstName'] = account['FirstName']
+            session['id'] = account['id']
+            session['usrname'] = account['usrname']
             session['email'] = account['email']
-
             return redirect(url_for('home'))
         else:
             msg = 'Incorrect username/password!'
 
-    return render_template('index.html', msg=msg)
-
+    return render_template('login.html', msg=msg)  # Ensured correct template is used
 
 # Logout Route
 @app.route('/logout')
@@ -70,19 +65,17 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-
 # Register Route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'firstname' in request.form and 'lastname' in request.form and 'password' in request.form and 'email' in request.form:
-        firstname = request.form['firstname']
-        lastname=request.form['lastname']
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
         password = request.form['password']
         email = request.form['email']
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM CUSTOMERS WHERE Email = %s', (email,))
+        cursor.execute('SELECT * FROM MyGuests WHERE usrname = %s', (username,))
         account = cursor.fetchone()
 
         if account:
@@ -91,11 +84,11 @@ def register():
             msg = 'Invalid email address!'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers!'
-        elif not firstname or not lastname or not password or not email:
+        elif not username or not password or not email:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO CUSTOMERS (FirstName, LastName, Email, Password) VALUES (%s, %s, %s, %s)', 
-                           (firstname, lastname, email, password,))
+            cursor.execute('INSERT INTO MyGuests (usrname, password, email) VALUES (%s, %s, %s)', 
+                           (username, password, email,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
 
@@ -103,7 +96,6 @@ def register():
         msg = 'Please fill out the form!'
 
     return render_template('register.html', msg=msg)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
